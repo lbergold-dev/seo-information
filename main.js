59c73ad652421c7e99275b2857d242e2db4ce83a
Leonhard Bergold <leonhard.bergold@outlook.com>
2026-01-04 17:16:29 +0100

    removed redundant DOMContentLoaded wrapper

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const colorSwitcher = document.querySelector('[data-color-switcher]');
-  const savedTheme = localStorage.getItem('theme') || 'light';
+const colorSwitcher = document.querySelector('[data-color-switcher]');
+const savedTheme = localStorage.getItem('theme') || 'light';
 
-  const setTheme = theme => {
-    document.documentElement.dataset.theme = theme;
-    document.documentElement.style.colorScheme = theme;
-    colorSwitcher.classList.toggle('color-switcher--dark', theme === 'dark');
-    colorSwitcher.classList.toggle('color-switcher--light', theme === 'light');
-  };
+const setTheme = theme => {
+  document.documentElement.dataset.theme = theme;
+  document.documentElement.style.colorScheme = theme;
+  colorSwitcher.classList.toggle('color-switcher--dark', theme === 'dark');
+  colorSwitcher.classList.toggle('color-switcher--light', theme === 'light');
+};
 
-  setTheme(savedTheme);
+setTheme(savedTheme);
 
-  colorSwitcher.addEventListener('click', () => {
-    const newTheme = document.documentElement.dataset.theme === 'dark' ? 'light' : 'dark';
-    setTheme(newTheme);
-    localStorage.setItem('theme', newTheme);
-  });
+colorSwitcher.addEventListener('click', () => {
+  const newTheme = document.documentElement.dataset.theme === 'dark' ? 'light' : 'dark';
+  setTheme(newTheme);
+  localStorage.setItem('theme', newTheme);
 });
